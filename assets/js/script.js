616b4af53390c48bd066ea74a3a38836eeeb651e
AinhoaBil <bl.ainhoa@gmail.com>
2022-02-21 22:38:27 +0000

    Add 5 onclick eventsfor the buttons in HTML. Started squeleton of the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Creating Game rules
+ * Creating Game rules convert
  */
 
  let player_hand_choice = {
@@ -45,3 +45,30 @@
 
     }
 };
+
+
+function player(hand){
+
+/* Array wiht weapons */
+let weapon =["Rock","Paper", "Scissors", "Lizard", "Spock"]; /* 0 to 4*/
+
+
+/* Variable to create a random number between 1 and 5 for the bot's selection, only the integer without decimals */
+let randNum = Math.trunc(Math.random() * 5); /* Verified on google dev tools it's working using console.log */
+
+/* Variable to create the bot choice converting numbers to weapons from the array above */
+let bot_choice = weapon[randNum]; /* Verified on google dev tools it's working using console.log */
+console.log(randNum);
+console.log(bot_choice);
+
+/* V to see who wins */
+let winner = document.getElementById("winner");
+
+
+/* Variable to count the score */
+
+/* Bot's choice and player's choice */
+
+/*Who wins text */
+
+} 
